b99d971b90527fce3766acfd8cc85d0da28a815d
mtchang <michaeltgchang@gmail.com>
2022-01-17 23:41:35 +0800

    pseudocode for Edit Page

--- src/components/EditPage/EditPage.js
+++ src/components/EditPage/EditPage.js
@@ -8,27 +8,37 @@ import { editPgActions } from "../../store/editPg";
 // import Component from "./fileName";
 
 const EditPage = () => {
-  // ALLOWS REDUX STORE TO BE ACCESSED
-  const dispatch = useDispatch();
+    // ALLOWS REDUX STORE TO BE ACCESSED
+    const dispatch = useDispatch();
 
-  // CALL STATES TO BE USED FROM STORE
-  // const storeStateName = useSelector((state) => state.mainPg.stateName);
+    // CALL STATES TO BE USED FROM STORE
+    // const storeStateName = useSelector((state) => state.mainPg.stateName);
 
-  // OTHER REDUCERS HERE, WITH USEEFFECT TO SPECIFY TRIGGER IF NECESSARY
+    // States needed for EDIT page:
+    // - reservation number (controlled HTML input)
+    // - all the data about reservation (controlled HTML inputs. Use the "New" as a template. Use "readonly" for the fields that impact reservation availability)
+    //    - is there a way to reuse the form? Maybe the reservation form can be a reuseable Component
 
-  // PAGE HTML TEMPLATE WITH COMPONENTS WHERE NECESSARY (STATIC PARTS ARE AS DEFINED IN MAINPAGE)
-  return (
-    <div>
-      EDIT PAGE
-      {/* ADD NAVLINK TO GO TO OTHER PAGES AS PER REQUIRED (CLASS NAME IS FOR ACTIVESTYLES) */}
-      {/* <NavLink
+    // Functions needed:
+    // - when reservation number is submitted, call the GET Booking and load the data
+    // - on submit edit, it will call the PATCH Booking endpoint
+    // - delete booking
+
+    // OTHER REDUCERS HERE, WITH USEEFFECT TO SPECIFY TRIGGER IF NECESSARY
+
+    // PAGE HTML TEMPLATE WITH COMPONENTS WHERE NECESSARY (STATIC PARTS ARE AS DEFINED IN MAINPAGE)
+    return (
+        <div>
+            EDIT PAGE
+            {/* ADD NAVLINK TO GO TO OTHER PAGES AS PER REQUIRED (CLASS NAME IS FOR ACTIVESTYLES) */}
+            {/* <NavLink
         className={(navData) => (navData.isActive ? styles.active : "")}
         to="/TARGETURL"
       >
         LINK TEXT
       </NavLink> */}
-    </div>
-  );
+        </div>
+    );
 };
 export default EditPage;
 
