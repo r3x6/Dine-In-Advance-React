204d5e6add78d948869e1ed413d9d323d5aa6e3c
mtchang <michaeltgchang@gmail.com>
2022-01-18 14:48:08 +0800

    retrieve and edit booking, save to server
    Can get data. Input is a placeholder textArea. Sends new data to server successfully to PATCH endpoint.

--- src/components/EditPage/EditPage.js
+++ src/components/EditPage/EditPage.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, NavLink } from "react-router-dom";
 
 import { editPgActions } from "../../store/editPg";
+
+const SERVER_URI = "https://dine-in-advance-server.herokuapp.com";
+//const SERVER_URI = "localhost:5000";
 
 // IMPORT NECESSARY COMPONENTS HERE
 // import Component from "./fileName";
 
 const EditPage = () => {
     const routerParams = useParams();
+
+    const [bookingDetails, setBookingDetails] = useState();
+    const [textAreaValue, setTextAreaValue] = useState();
 
     // ALLOWS REDUX STORE TO BE ACCESSED
     const dispatch = useDispatch();
@@ -22,33 +28,65 @@ const EditPage = () => {
     //    - is there a way to reuse the form? Maybe the reservation form can be a reuseable Component
 
     // Functions needed:
-    // - on page load, call the GET Booking and load the data
-    // - on submit edit, it will call the PATCH Booking endpoint
-    // - delete booking
 
     // OTHER REDUCERS HERE, WITH USEEFFECT TO SPECIFY TRIGGER IF NECESSARY
 
-    // get data on page load
+    // on page load, call the GET Booking and load the data
     useEffect(() => {
         console.log(`Fetching data for bookingId: ${routerParams.bookingId}`);
 
-        // MICHAEL LEFT OFF HERE
         async function fetchBooking() {
             const response = await fetch(
-                `https://dine-in-advance-server.herokuapp.com/api/booking?id=${routerParams.bookingId}`
+                `${SERVER_URI}/api/booking?id=${routerParams.bookingId}`
             );
             const myJson = await response.json();
-            // do something with the data, like assign it to a State or Reducer
-            console.log("response:", myJson);
+            setBookingDetails(myJson);
+            setTextAreaValue(JSON.stringify(myJson));
         }
 
         fetchBooking();
     }, []);
 
+    // on submit edit, it will call the PATCH Booking endpoint
+    function handleEditClick(e) {
+        // send to API endpoint
+        async function patchBooking() {
+            const response = await fetch(
+                `${SERVER_URI}/api/booking?id=${routerParams.bookingId}`,
+                { method: "PATCH", body: JSON.stringify(bookingDetails) }
+            );
+            const myJson = await response.json();
+            // do something with PATCH response
+            console.log(myJson);
+        }
+
+        try {
+            const obj = JSON.parse(textAreaValue);
+            patchBooking();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    // delete booking
+
     // PAGE HTML TEMPLATE WITH COMPONENTS WHERE NECESSARY (STATIC PARTS ARE AS DEFINED IN MAINPAGE)
     return (
         <div>
-            EDIT PAGE
+            <h3>Edit Booking</h3>
+            <textarea
+                rows="15"
+                cols="50"
+                value={textAreaValue}
+                onChange={(e) => {
+                    setTextAreaValue(e.target.value);
+                }}
+            />
+            <div>
+                <button onClick={handleEditClick}>Save Changes</button>
+                <button>Delete</button>
+            </div>
+
             {/* ADD NAVLINK TO GO TO OTHER PAGES AS PER REQUIRED (CLASS NAME IS FOR ACTIVESTYLES) */}
             {/* <NavLink
         className={(navData) => (navData.isActive ? styles.active : "")}
